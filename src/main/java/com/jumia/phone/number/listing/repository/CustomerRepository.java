b3c6e34d60151622a9624b24aec529f5612e36f2
oswidan97 <oswidan97@gmail.com>
2021-11-20 20:10:52 +0200

    Implement the specification interface

--- src/main/java/com/jumia/phone/number/listing/repository/CustomerRepository.java
+++ src/main/java/com/jumia/phone/number/listing/repository/CustomerRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer>, JpaSpecificationExecutor<Customer> {
   Page<Customer> findAllByCountry(Country country, Pageable pageable);
 
   Page<Customer> findAllByState(boolean state, Pageable pageable);
