7e2e19eebbc112f2a0e5c8416adb772ee749a888
oswidan97 <oswidan97@gmail.com>
2021-11-21 17:44:35 +0200

    Remove redundancy

--- src/main/java/com/jumia/phone/number/listing/entity/Customer.java
+++ src/main/java/com/jumia/phone/number/listing/entity/Customer.java
@@ -22,6 +22,14 @@ public class Customer {
   @OneToOne
   private Country country;
 
+  public Customer() {
+  }
+
+  public Customer(String name, String phone, boolean validPhone) {
+    this.name = name;
+    this.phone = phone;
+    this.validPhone = validPhone;
+  }
 
   public Integer getId() {
     return id;
@@ -31,7 +39,45 @@ public class Customer {
     this.id = id;
   }
 
-  public boolean isPhoneNumberValid() {
-    return Pattern.matches(country.getRegex(), phone);
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public boolean isValidPhone() {
+    return validPhone;
+  }
+
+  public void setValidPhone(boolean validPhone) {
+    this.validPhone = validPhone;
+  }
+
+  public Country getCountry() {
+    return country;
+  }
+
+  public void setCountry(Country country) {
+    this.country = country;
+  }
+
+  public void validatePhoneNumber() {
+    validPhone = Pattern.matches(country.getRegex(), phone);
+  }
+
+  @Override
+  public String toString() {
+    return "Customer{" + "id=" + id + ", name='" + name + '\'' + ", phone='" + phone + '\'' + ", validPhone=" + validPhone + ", country=" + country +
+            '}';
   }
 }
--- src/main/java/com/jumia/phone/number/listing/repository/CustomerRepository.java
+++ src/main/java/com/jumia/phone/number/listing/repository/CustomerRepository.java
@@ -1,18 +1,10 @@
 package com.jumia.phone.number.listing.repository;
 
-import com.jumia.phone.number.listing.entity.Country;
 import com.jumia.phone.number.listing.entity.Customer;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>, JpaSpecificationExecutor<Customer> {
-  Page<Customer> findAllByCountry(Country country, Pageable pageable);
-
-  Page<Customer> findAllByState(boolean state, Pageable pageable);
-
-
 }
